80904726ed404bb6d6a11e1aab5f7564d9c0155f
madzit1 <madzitsadoev1@gmail.com>
2024-06-01 00:45:20 +0300

    added index, works, hero, advantages, services, team, partners, head

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,11 @@
 $(function(){
+    
+    $('.hero__slider').slick({
+        dots: false,
+        arrows: false,
+        autoplay: true
+    });
+
+
     var mixer = mixitup('.works__list');
 });
